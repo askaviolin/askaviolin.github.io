28269875c775e3b269b64ab58ef6bbbc7f641f2d
askaviolin <ilan.isakov@gmail.com>
2023-05-24 13:11:04 -0400

    Create menu.php

---
+++ includes/menu.php
@@ -0,0 +1,6 @@
+<ul><a href=""><img class="resize" src="hh.png" alt="Harmonie Hall Home"></a>
+		<a href="index.html" class="header">Home</a>
+		<a href="events.html" class="header">Events</a>
+		<a href="map.html" class="header">Map</a>
+		<a href="contact.html" class="header">Contact</a>
+	</ul>
